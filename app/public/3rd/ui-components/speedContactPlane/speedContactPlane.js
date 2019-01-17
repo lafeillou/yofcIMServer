f1ecc44a8b12d7e9379ec15eb1ee0e3a543f9503
louyongliang <lafeilou@126.com>
2019-01-17 21:02:37 +0800

    右下角快速联系常驻功能

--- app/public/3rd/ui-components/speedContactPlane/speedContactPlane.js
+++ app/public/3rd/ui-components/speedContactPlane/speedContactPlane.js
@@ -1,13 +1,20 @@
 define(function (require, exports, module) {
     var ejs = require('ejs');
     var $ = require('jquery');
-    var template = require('./speedContactPlane.tpl');
-    alert(template);
-
+    var template = require('./speedContactPlane.html');
     module.exports = {
         initPlane: function () {
-            let people = ['geddy', 'neil', 'alex'],
-                html = ejs.render('<%= people.join(", "); %>', { people: people });
-        }
+            // let people = ['geddy', 'neil', 'alex'],
+            //     html = ejs.render('<%= people.join(", "); %>', { people: people });
+            this.$speedContactPlane = $(template);
+            $('body').append(this.$speedContactPlane);
+
+            // 点击展开面板
+            this.$speedContactPlane.find('a.speedContactButton').on('click',function(){
+                alert('ok');
+            });
+        },
+
+
     }
 });
