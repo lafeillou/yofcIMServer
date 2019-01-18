60cb69419d7ab53963cb79efee0f8463a20241da
louyongliang <lafeilou@126.com>
2019-01-18 11:59:24 +0800

    打开关闭右下角快速联系面板

--- app/public/3rd/ui-components/speedContactPlane/speedContactPlane.js
+++ app/public/3rd/ui-components/speedContactPlane/speedContactPlane.js
@@ -7,12 +7,17 @@ define(function (require, exports, module) {
             // let people = ['geddy', 'neil', 'alex'],
             //     html = ejs.render('<%= people.join(", "); %>', { people: people });
             this.$speedContactPlane = $(template);
+            // 联系人列表显示容器、联系人搜索框
             $('body').append(this.$speedContactPlane);
-
+            this.$contactListWrap = this.$speedContactPlane.find('.contactListWrap');
             // 点击展开面板
-            this.$speedContactPlane.find('a.speedContactButton').on('click',function(){
-                alert('ok');
-            });
+            this.$speedContactPlane.find('a.speedContactButton').on('click', function () {
+                if (this.$contactListWrap.hasClass('show')) {
+                    this.$contactListWrap.removeClass('show');
+                } else {
+                    this.$contactListWrap.addClass('show');
+                }
+            }.bind(this));
         },
 
 
