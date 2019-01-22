86ce6fdb2291d6ed9316f85e03aefc6a48e86133
louyongliang <lafeilou@126.com>
2019-01-22 11:55:25 +0800

    集成layer弹窗组件

--- app/public/3rd/ui-components/speedContactPlane/speedContactPlane.js
+++ app/public/3rd/ui-components/speedContactPlane/speedContactPlane.js
@@ -2,6 +2,8 @@ define(function (require, exports, module) {
     var ejs = require('ejs');
     var $ = require('jquery');
     var template = require('./speedContactPlane.html');
+    var layer = require('layer');
+
     module.exports = {
         initPlane: function () {
             // let people = ['geddy', 'neil', 'alex'],
@@ -10,6 +12,9 @@ define(function (require, exports, module) {
             // 联系人列表显示容器、联系人搜索框
             $('body').append(this.$speedContactPlane);
             this.$contactListWrap = this.$speedContactPlane.find('.contactListWrap');
+            // 主功能按钮（新消息、建群、设置）
+            this.$speedContactFnBtns = this.$speedContactPlane.find('#speedContactFnBtns');
+
             // 点击展开面板
             this.$speedContactPlane.find('a.speedContactButton').on('click', function () {
                 if (this.$contactListWrap.hasClass('show')) {
@@ -18,6 +23,25 @@ define(function (require, exports, module) {
                     this.$contactListWrap.addClass('show');
                 }
             }.bind(this));
+
+            this.$speedContactFnBtns.on('click', function (e) {
+                e.stopPropagation();
+                var fnCode = $(e.target).data('fncode');
+
+                //if (fnCode === 1) {
+                layer.open({
+                    type: 2,
+                    area: ['700px', '450px'],
+                    fixed: false, //不固定
+                    maxmin: true,
+                    content: 'http://www.baidu.com'
+                });
+                //}
+            });
+
+            // layer.ready(function () {
+            //     layer.msg('我爱你');
+            // });
         },
 
 
--- app/public/3rd/yofcMessenger-sdk.js
+++ app/public/3rd/yofcMessenger-sdk.js
@@ -25,34 +25,38 @@
     }
 
     yofcMessenger.init = function (cb) {
-        
+
         loadScript('http://localhost:7001/public/3rd/seajs/2.2.3/sea.js', function () {
             loadScript('http://localhost:7001/public/3rd/seajs/plugins/seajs-text.js', function () {
-                 //配置seajs
-            seajs.config({
-                alias: {
-                    "nimWebNetCall": "sdk/NIM_Web_Netcall_Module_v5.6.0",
-                    "nimWebSDK": "sdk/NIM_Web_SDK_Module_v5.6.0",
-                    "speedContactFn": "ui-components/speedContactPlane/speedContactPlane",
-                    //这里用的jquery同发布者环境用的jquery是独立的，比如发布者环境的window.jquery的版本是3.3.1,而这里是2.2.4
-                    "jquery": "sdk/jquery_v2.2.4_Module",
-                    //模板引擎
-                    "ejs": "sdk/ejs_v2.5.8_Module"
-                },
-                // Sea.js 的基础路径
-                base: 'http://localhost:7001/public/3rd/',
-            });
+                //配置seajs
+                seajs.config({
+                    alias: {
+                        "nimWebNetCall": "sdk/NIM_Web_Netcall_Module_v5.6.0",
+                        "nimWebSDK": "sdk/NIM_Web_SDK_Module_v5.6.0",
+                        "speedContactFn": "ui-components/speedContactPlane/speedContactPlane",
+                        //这里用的jquery同发布者环境用的jquery是独立的，比如发布者环境的window.jquery的版本是3.3.1,而这里是2.2.4
+                        "jquery": "sdk/jquery_v2.2.4_Module",
+                        //模板引擎
+                        "ejs": "sdk/ejs_v2.5.8_Module",
+                        "layer": "sdk/ui/layer/3.1.1/layer"
+                    },
+                    // Sea.js 的基础路径
+                    base: 'http://localhost:7001/public/3rd/',
+                });
 
-            seajs.use(['nimWebNetCall', 'nimWebSDK', 'speedContactFn', 'theme/default/css/main.css'], function (nimWebNetCall, nimWebSDK, speedContactFn) {
-                if (cb && typeof cb === 'function') {
-                    cb();
-                }
-
-                speedContactFn.initPlane();
-                console.log(nimWebNetCall);
-                console.log(nimWebSDK);
+                seajs.use(['nimWebNetCall', 'nimWebSDK', 'speedContactFn', 'layer', 'theme/default/css/main.css'], function (nimWebNetCall, nimWebSDK, speedContactFn, layer) {
+                    if (cb && typeof cb === 'function') {
+                        cb();
+                    }
+                    // 以下代码直接写到了layer源代码里
+                    // layer.config({
+                    //     path: 'http://localhost:7001/public/3rd/sdk/ui/layer/3.1.1/'
+                    // });
+                    speedContactFn.initPlane();
+                    // console.log(nimWebNetCall);
+                    // console.log(nimWebSDK);
+                });
             });
-        });
         });
     }
 
