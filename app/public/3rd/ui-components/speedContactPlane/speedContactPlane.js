781799f6b69d67f9cc2d4f2dba4420e8e1d9618e
louyongliang <lafeilou@126.com>
2019-01-22 19:11:08 +0800

    新建群弹窗样式编写

--- app/public/3rd/ui-components/speedContactPlane/speedContactPlane.js
+++ app/public/3rd/ui-components/speedContactPlane/speedContactPlane.js
@@ -4,6 +4,7 @@ define(function (require, exports, module) {
     var template = require('./speedContactPlane.html');
     var layer = require('layer');
 
+    var currentIndex = -1;
     module.exports = {
         initPlane: function () {
             // let people = ['geddy', 'neil', 'alex'],
@@ -27,23 +28,51 @@ define(function (require, exports, module) {
             this.$speedContactFnBtns.on('click', function (e) {
                 e.stopPropagation();
                 var fnCode = $(e.target).data('fncode');
+                switch (fnCode) {
+                    case 0:
+                        // this.openAddChartMemberDialog();
+                        break;
+                    case 1:
+                        this.openAddChartMemberDialog();
+                        break;
+                    case 2:
+                        break;
+                    default:
+                        console.log('error');
+                        break;
 
-                //if (fnCode === 1) {
-                layer.open({
-                    type: 2,
-                    area: ['700px', '450px'],
-                    fixed: false, //不固定
-                    maxmin: true,
-                    content: 'http://www.baidu.com'
-                });
-                //}
-            });
+                }
 
-            // layer.ready(function () {
-            //     layer.msg('我爱你');
-            // });
+            }.bind(this));
         },
 
-
+        // 打开新建群聊弹窗
+        openAddChartMemberDialog: function () {
+            var template = require('./addChartMemberDialog.html');
+            currentIndex = layer.open({
+                // type: 2,
+                // area: ['700px', '450px'],
+                // fixed: false, //不固定
+                // maxmin: true,
+                // content: 'http://www.baidu.com'
+                type: 1,
+                title: ['创建群组', 'font-size:18px;'],
+                skin: 'yofcMsgr',
+                area: ['650px', '600px'],
+                resize: false,
+                content: template,
+                btn: ['创建', '取消'],
+                scrollbar: false,
+                yes: function (index, layero) {
+                    // alert('点击了取消');
+                    // return true;
+                    alert('点击了创建');
+                    return false;
+                },
+                btn2: function (index, layero) {
+                    layer.close(currentIndex);
+                }
+            });
+        }
     }
 });
